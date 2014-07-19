7d2a8d177acbdbc9db7a90b985e7995b5862bb0c
DSRoden <Daniel.S.Roden@gmail.com>
2014-07-19 18:54:40 -0500

    Discover Meteor Sample App

--- collections/comments.js
+++ collections/comments.js
@@ -1,54 +1,34 @@
-// Fixture data 
-if (Posts.find().count() === 0) {
-  var now = new Date().getTime();
+Comments = new Meteor.Collection('comments');
 
-  // create two users
-  var tomId = Meteor.users.insert({
-    profile: { name: 'Tom Coleman' }
-  });
-  var tom = Meteor.users.findOne(tomId);
-  var sachaId = Meteor.users.insert({
-    profile: { name: 'Sacha Greif' }
-  });
-  var sacha = Meteor.users.findOne(sachaId);
-
-  var telescopeId = Posts.insert({
-    title: 'Introducing Telescope',
-    userId: sacha._id,
-    author: sacha.profile.name,
-    url: 'http://sachagreif.com/introducing-telescope/',
-    submitted: now - 7 * 3600 * 1000
-  });
-
-  Comments.insert({
-    postId: telescopeId,
-    userId: tom._id,
-    author: tom.profile.name,
-    submitted: now - 5 * 3600 * 1000,
-    body: 'Interesting project Sacha, can I get involved?'
-  });
-
-  Comments.insert({
-    postId: telescopeId,
-    userId: sacha._id,
-    author: sacha.profile.name,
-    submitted: now - 3 * 3600 * 1000,
-    body: 'You sure can Tom!'
-  });
-
-  Posts.insert({
-    title: 'Meteor',
-    userId: tom._id,
-    author: tom.profile.name,
-    url: 'http://meteor.com',
-    submitted: now - 10 * 3600 * 1000
-  });
-
-  Posts.insert({
-    title: 'The Meteor Book',
-    userId: tom._id,
-    author: tom.profile.name,
-    url: 'http://themeteorbook.com',
-    submitted: now - 12 * 3600 * 1000
-  });
-}
+Meteor.methods({
+  comment: function(commentAttributes) {
+    var user = Meteor.user();
+    var post = Posts.findOne(commentAttributes.postId);
+    // ensure the user is logged in
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to make comments");
+      
+    if (!commentAttributes.body)
+      throw new Meteor.Error(422, 'Please write some content');
+      
+    if (!post)
+      throw new Meteor.Error(422, 'You must comment on a post');
+    
+    comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+    
+    // update the post with the number of comments
+    Posts.update(comment.postId, {$inc: {commentsCount: 1}});
+    
+    // create the comment, save the id
+    comment._id = Comments.insert(comment);
+    
+    // now create a notification, informing the user that there's been a comment
+    createCommentNotification(comment);
+    
+    return comment._id;
+  }
+});
